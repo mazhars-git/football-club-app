ed1a485ab437f3ba4ff61e91fc0092acdf970bfd
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-03-10 15:05:01 +0600

    update

--- src/component/AllTeam/AllTeam.js
+++ src/component/AllTeam/AllTeam.js
@@ -4,9 +4,10 @@ import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
 const AllTeam = (props) => {
-    const {strTeam, strSport, strLeague, idTeam} = props.team;
+    const {strTeam, strSport, strLeague, idTeam, strTeamBadge} = props.team;
     return (
         <div className="all-team-box">
+            <img src={strTeamBadge} alt=""/> 
             <h3>Team Name: {strTeam}</h3>
             <p>{strSport}</p>
             <p>{strLeague}</p>
--- src/component/Team/Team.js
+++ src/component/Team/Team.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const Team = () => {
+    const {idTeam} = useParams();
+    const [team, setTeam] = useState({});
+    const {strDescriptionEN, strGender, strLeague, strTeamLogo, strTeamBadge} = team;
+    useEffect(() => {
+        const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${idTeam}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setTeam(data.teams[0]))
+    }, [])
+
     return (
         <div>
-            <h1>Team Detail</h1>
+            <h1>Team Detail of {idTeam}</h1>
+            <h3>{strLeague}</h3>
+            <p>Gender: {strGender}</p>
+            <img src={strTeamBadge} alt=""/>
+            <p>Description: {strDescriptionEN}</p>
         </div>
     );
 };
